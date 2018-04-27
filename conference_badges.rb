0331a5ef16ec2e8984bd92133b0d430c6c28b25b
Emily Jirles <emily.jirles@gmail.com>
2018-04-27 17:49:22 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,16 @@
 # Write your code here.
 def badge_maker(name)
   "Hello, my name is #{name}."
+end 
+
+def batch_badge_creater(name_array)
+  name_array.collect{|name| badge_maker(name)}
+end
+
+def assign_rooms(name_array)
+  room_assignmetns = []
+end
+
+def printer
+  
+end
